6c8fa070f7312fd56a10707831c6f3202e4e989b
romain.rousval <romain.rousval@depinfoad.univ-brest.fr>
2024-12-02 11:56:29 +0100

    add test

---
+++ src/test/java/org/simulation/etats/AdulteTest.java
@@ -0,0 +1,12 @@
+package org.simulation.etats;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdulteTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
