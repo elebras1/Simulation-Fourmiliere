1f80fdb65556cfe829dc05c1dfa5bc32c8bade22
erwan.lebras <erwan.lebras@depinfoad.univ-brest.fr>
2024-12-05 13:41:55 +0100

    correction

--- src/main/java/org/simulation/vue/VuParameters.java
+++ src/main/java/org/simulation/vue/VuParameters.java
@@ -13,33 +13,33 @@ public class VuParameters extends VueElement {
         space.add(sliderWidth);
         space.setLayout(null);
 
-        JLabel labelHeure = this.createLabel("Heure:", 100, space);
+        this.createLabel("Heure:", 100, space);
         JTextField tfHeure = this.createTextField(String.valueOf(simulation.getSaisons().getHeure()), 100, space);
         simulation.getTimer().addActionListener(e -> tfHeure.setText(String.valueOf(simulation.getSaisons().getHeure())));
         tfHeure.setEditable(false);
 
-        JLabel labelProbabiliteOuvriere = this.createLabel("Probabilité Ouvrière:", 150, space);
+        this.createLabel("Probabilité Ouvrière:", 150, space);
         JTextField tfProbabiliteOuvriere = this.createTextField(String.valueOf(Parameters.PROBABILITE_OUVRIERE), 150, space);
 
-        JLabel labelProbabiliteSoldat = this.createLabel("Probabilité Soldat:", 200, space);
+        this.createLabel("Probabilité Soldat:", 200, space);
         JTextField tfProbabiliteSoldat = this.createTextField(String.valueOf(Parameters.PROBABILITE_SOLDAT), 200, space);
 
-        JLabel labelProbabiliteIndividuSexue = this.createLabel("Probabilité Individu Sexué:", 250, space);
+        this.createLabel("Probabilité Individu Sexué:", 250, space);
         JTextField tfProbabiliteIndividuSexue = this.createTextField(String.valueOf(Parameters.PROBABILITE_INDIVIDU_SEXUE), 250, space);
 
-        JLabel labelMaximumReines = this.createLabel("Maximum Reines:", 300, space);
+        this.createLabel("Maximum Reines:", 300, space);
         JTextField tfMaximumReines = this.createTextField(String.valueOf(Parameters.MAXIMUM_REINES), 300, space);
 
-        JLabel labelAgeLarve = this.createLabel("Âge Larve:", 350, space);
+        this.createLabel("Âge Larve:", 350, space);
         JTextField tfAgeLarve = this.createTextField(String.valueOf(Parameters.AGE_LARVE), 350, space);
 
-        JLabel labelAgeNymphe = this.createLabel("Âge Nymphe:", 400, space);
+        this.createLabel("Âge Nymphe:", 400, space);
         JTextField tfAgeNymphe = this.createTextField(String.valueOf(Parameters.AGE_NYMPHE), 400, space);
 
-        JLabel labelAgeAdulte = this.createLabel("Âge Adulte:", 450, space);
+        this.createLabel("Âge Adulte:", 450, space);
         JTextField tfAgeAdulte = this.createTextField(String.valueOf(Parameters.AGE_ADULTE), 450, space);
 
-        JLabel labelBiaisMinimal = this.createLabel("Biais Minimal:", 500, space);
+        this.createLabel("Biais Minimal:", 500, space);
         JTextField tfBiaisMinimal = this.createTextField(String.valueOf(Parameters.BIAIS_MINIMAL), 500, space);
 
         JButton applyButton = new JButton("Appliquer");
@@ -54,7 +54,6 @@ public class VuParameters extends VueElement {
                 Parameters.AGE_NYMPHE = Integer.parseInt(tfAgeNymphe.getText());
                 Parameters.AGE_ADULTE = Integer.parseInt(tfAgeAdulte.getText());
                 Parameters.BIAIS_MINIMAL = Double.parseDouble(tfBiaisMinimal.getText());
-
             } catch (NumberFormatException ex) {
                 JOptionPane.showMessageDialog(space, "Erreur : Veuillez entrer des valeurs valides.");
             }
